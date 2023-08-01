81210443fae872362f46e59e9a3c4b69783f38a6
Max Pugh <max.pugh@illuminetsolutions.com>
2023-08-01 13:35:08 +0100

    edited api url

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'https://quiz-api-final-env.eba-hcvprz3y.eu-north-1.elasticbeanstalk.com/';
 
     return service;
 }]);
